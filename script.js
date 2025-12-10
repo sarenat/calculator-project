3c083fe13db2e47865b34dd558c4262c91f183f5
sarenat <sarenattran@gmail.com>
2025-12-09 19:12:26 -0500

    Implement calculator logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add (x, y) {
-    return x + y;
+    return Number(x) + Number(y);
 }
 function subtract (x, y) {
     return x - y;
@@ -13,9 +13,9 @@ function divide (x, y) {
 
 let operatorOne = null;
 let operatorTwo = null;
-let operation = "";
+let operation = null;
 
-function operate (operatorOne, operatorTwo, operation) {
+function operate (operatorOne, operation, operatorTwo) {
     switch (operation) {
         case "x": return multiply(operatorOne, operatorTwo);
         case "+": return add(operatorOne, operatorTwo);
@@ -27,26 +27,92 @@ function operate (operatorOne, operatorTwo, operation) {
 const display = document.querySelector(".display");
 let displayValue = document.querySelector(".display-value");
 
+function updateOperators (operatorNumber, input) {
+    switch (operatorNumber) {
+        case 1: 
+            operatorOne = input;
+            break;
+        case 2:
+            operatorTwo = input;
+            break;
+        case 3:
+            operatorOne += input;
+            break;
+        case 4:
+            operatorTwo += input;
+            break;
+    };
+};
+function updateOperation (input) {
+    operation = input;
+};
+function updateDisplay() {
+
+}
+function clearDisplay () {
+    operatorOne = null;
+    operatorTwo = null; 
+    operation = null;
+
+    display.removeChild(displayValue);
+    displayValue.textContent = 0;
+    display.appendChild(displayValue);
+}
+function evaluate (opOne, op, opTwo, input) {
+    if (!opOne && !op && !opTwo) { // Scenario one
+        if (!isNaN(input)) { // if input is #
+            updateOperators(1, input);
+        }
+    } else if (opOne && !op && !opTwo) { // Scenario two
+        if (!isNaN(input)) { // if input is #
+            updateOperators(3, input);
+        }
+        if (isNaN(input)) { // if input is string
+            updateOperation(input);
+        }
+    } else if (opOne && op && !opTwo) { // Scenario three
+        if (!isNaN(input)) { // if input is #
+            updateOperators(2, input);
+        }
+        if (isNaN(input)) { // if input is string
+            if (input != "=") {
+                updateOperation(input);
+            }
+        }
+    } else if (opOne && op && opTwo) { // Scenario four 
+        if (!isNaN(input)) { // if input is #
+            updateOperators(4, input);
+        }
+        if (isNaN(input)) { // if input is string
+            const newOperatorOne = operate(opOne, op, opTwo);
+            operatorOne = newOperatorOne;
+
+            operatorTwo = null;
+            operation = null;
+
+            if (input != "=") {
+                updateOperation(input);
+            }
+        }
+    }
+}
+
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
-        display.removeChild(displayValue);
-        if (displayValue.textContent != 0) displayValue.textContent += button.textContent;
-        else displayValue.textContent = button.textContent;
-        display.appendChild(displayValue);
+        const input = button.textContent;
+        evaluate(operatorOne, operation, operatorTwo, input);
     });
 });
 
 const operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach(button => {
     button.addEventListener("click", () => {
-        operation = button.textContent;
+        const input = button.textContent; 
+        evaluate(operatorOne, operation, operatorTwo, input);
     });
 });
 
 const clearButton = document.querySelector(".clear");
-clearButton.addEventListener("click", () => {
-    display.removeChild(displayValue);
-    displayValue.textContent = 0;
-    display.appendChild(displayValue);
-});
+clearButton.addEventListener("click", () => clearDisplay());
+
